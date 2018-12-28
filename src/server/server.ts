cd7a50fe23b805ce9c5b46bb12521b92e7799bba
Unknown <carlossas_97@hotmail.com>
2018-12-28 10:07:43 -0600

    v2.0

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,34 +1,88 @@
-//CLASE PARA INICIAR UN SERVIDOR EXPRESS EN UN PUERTO ESPECIFICO
 
-import express = require('express');
-import path = require('path');
+//LIBRERIA EXPRESS
+import express from 'express';
+//VARIABLES GLOBALES (PUERTO)
+import { SERVER_PORT } from '../global/environment';
+//LIBRERIA SOCKETS
+import socketIO from 'socket.io';
+//HTTP
+import http from 'http';
+//CORS( Configura quien puede realizar pedidos a la api )
+import cors from 'cors';
+//BODY PARSER ( Permite usar envio de formularios en JSON )
+import bodyParser from 'body-parser';
+//PATH
+import path from 'path';
+//FUNCIONES DE SOCKET
+import * as socketService from '../sockets/sockets'
 
-export default class Server{
+export default class Server {
+    private static _instance: Server;
 
     public app: express.Application;
     public port: number;
+    public io: socketIO.Server;
+    private httpServer : http.Server;
 
-    constructor(
-        puerto: number
-    ){  
-        //INICIALIZAMOS EL PUERTO Y EL SERVIDOR EXPRESS
-        this.port = puerto;
+
+    private constructor() {
+        //SERVIDOR EXPRESS
         this.app = express();
+        //PUERTO DE LA APP
+        this.port = SERVER_PORT;
+        //SERVIDOR HTTP
+        this.httpServer = new http.Server( this.app );
+        //OBJETO IO DE SOCKET
+        this.io = socketIO( this.httpServer );
+        //OPCIONES DE CORS
+        this.cors();
+        //BODY PARSE
+        this.bodyParser();
+        //INICIAR EL SERVICIO DE SOCKET
+        this.escucharSockets();
+    }
+
+    //RETORNA LA INSTANCIA DE LA CLASE O CREA UNA NUEVA SI NO EXISTE
+    public static get instance(){
+        return this._instance || ( this._instance = new this() );
+    }
+
+    //ESCUCHAR SOCKETS
+    private escucharSockets(){
+        console.log("escuchando conexiones");
+        this.io.on('connection', cliente =>{
+
+            console.log("conectado a socket");
+              
+
+            //Desconectar cliente
+            socketService.desconectar( cliente );
+
+        });
     }
     
-    static init (puerto:number){
-        return new Server ( puerto );
-    }
-
     //FUNCION PARA ACCEDER AL FOLDER PUBLIC
     private publicFolder(){
         const publicPath = path.resolve(__dirname, '../public');
         this.app.use( express.static(publicPath) );
     }
 
-    start( callback: Function ){
-        this.app.listen(this.port, callback);
-        this.publicFolder();
+    //BODYPARSE 
+    private bodyParser(){
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(bodyParser.json());
+    }
+
+    //CONFIGURACION DE CORS
+    private cors(){
+        this.app.use( cors({ origin: true, credentials: true  }) );
+    }
+
+    //INICIAR EL SERVIDOR
+    start( callback: Function ) {
+
+        this.httpServer.listen( this.port, callback );
+
     }
 
 }
---
+++ src/sockets/sockets.ts
@@ -0,0 +1,14 @@
+import { Socket } from 'socket.io'
+import socketIO from 'socket.io';
+
+//Desconexión
+export const desconectar = ( cliente: Socket )=>{
+
+    cliente.on('disconnect', ()=>{
+        console.log("Cliente desconectado");
+
+        
+    });
+
+}
+
